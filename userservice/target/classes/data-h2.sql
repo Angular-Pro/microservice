d882bcea68637a019de3ed23a649684511f04f79
czhang <czhang@lexon-corp.com>
2018-10-27 20:32:07 -0400

    delete compiled class

--- userservice/target/classes/data-h2.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO user (username, first_name, last_name, password) VALUES
-  ('lala', 'lala', 'lala', 'lala'),
-  ('lolo', 'lolo', 'lolo', 'lolo');
-
-INSERT INTO role (role_id, name) VALUES
-  (0, 'ROLE_ADMIN'),
-  (1, 'ROLE_USER');
